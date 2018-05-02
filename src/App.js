1bcb977f1ce866a46a60fdc04c9e0aee0ed097c0
Patrick Scherler <pacs01dev@gmail.com>
2018-05-02 23:24:26 +0200

    add background image and reformat code

--- src/App.js
+++ src/App.js
@@ -1,40 +1,48 @@
 import React, {Component} from 'react';
 import ReactHeatmap from './ReactHeatmap';
 import './App.css';
+import Background from './images/floor-plan.png';
 
 class App extends Component {
 
-  constructor() {
-    super();
-    this.state = {
-      readings: [],
-    };
-  }
+    constructor() {
+        super();
+        this.state = {
+            readings: [],
+        };
+    }
 
-  componentDidMount() {
-    fetch('http://localhost:9000/measurements').then(results => {
-      return results.json();
-    }).then(data => {
-      let readings = data[0].readings.map((reading) => {
-        let x = reading.xposition * 100;
-        let y = reading.yposition * 100;
-        let value = reading.luxValue * 100;
-        console.log(x + " : " + y + " : " + value);
-        return {x: x, y: y, value: value};
-      });
-      this.setState({readings: readings});
-    })
-  }
+    componentDidMount() {
+        fetch('http://localhost:9000/measurements').then(results => {
+            return results.json();
+        }).then(data => {
+            let readings = data[0].readings.map((reading) => {
+                let x = reading.xposition * 100;
+                let y = reading.yposition * 100;
+                let value = reading.luxValue * 100;
+                console.log(x + " : " + y + " : " + value);
+                return {x: x, y: y, value: value};
+            });
+            this.setState({readings: readings});
+        })
+    }
 
-  render() {
-    return (
-        <div className="App">
-          <div style={{width: window.innerWidth, height: window.innerHeight}}>
-            <ReactHeatmap max={100} data={this.state.readings} />
-          </div>
-        </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <div style={{
+                    width: window.innerWidth,
+                    height: window.innerHeight,
+                    backgroundImage: "url(" + Background + ")",
+                    backgroundPosition: "center",
+                    backgroundRepeat: "no-repeat",
+                    backgroundSize: "contain"
+                }}>
+                    <ReactHeatmap max={100} data={this.state.readings}/>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
